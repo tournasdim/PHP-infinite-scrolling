062d7e97a3ec00864211ca4994f08ed4f06638ed
Tim Groeneveld <tim@timg.ws>
2013-06-13 15:28:12 +1000

    Removed custom_jquery-debug.js, replaced with updated custom_jquery that has a debug flag on it. Compiles down to less then 1KB w/ Google Closure compiler.

--- javascript/custom_jquery.js
+++ javascript/custom_jquery.js
@@ -1,42 +1,50 @@
-$(document).ready(function() { 
- $("#postedComments").append( "<p id='last'></p>" );
-//console.log("Document Ready");
-  doMouseWheel = 1 ;   
-$(window).scroll(function() {
-//console.log("Window Scroll ----");
+_debug = false;
 
-if (!doMouseWheel)  {
-return ;
-} ;
- var distanceTop = $('#last').offset().top - $(window).height();	
-	if  ($(window).scrollTop() > distanceTop){
-//console.log("Window distanceTop to scrollTop Start");
- doMouseWheel = 0 ; 
-	$('div#loadMoreComments').show();
-	//console.log("Another window to the end !!!! "+$(".postedComment:last").attr('id'));	
- 	$.ajax({
-	dataType : "html" ,
-	url: "jquery-loadMoreComments.php?lastComment="+ $(".postedComment:last").attr('id') ,	
-	success: function(html) {
-	 doMouseWheel = 1 ; 
-	if(html){
-	$("#postedComments").append(html);
-	//console.log("Append html--------- " +$(".postedComment:first").attr('id'));
-	//console.log("Append html--------- " +$(".postedComment:last").attr('id'));
-	$("#last").remove();
-	$("#postedComments").append( "<p id='last'></p>" );
-	$('div#loadMoreComments').hide();
-	}else{		
-	$('div#loadMoreComments').replaceWith("<center><h1 style='color:red'>End of countries !!!!!!!</h1></center>");
-// Added on Ver.0.4  
-//Disable Ajax when result from PHP-script is empty (no more DB-results )
-	 doMouseWheel = 0 ; 
-	}
-	}
-	});
-	}
-	});
-	});
-	
-	
-	
+function dbg(msg) {
+    if (_debug)
+        console.log (msg);
+}
+
+$(document).ready(function() {
+    doMouseWheel = 1 ;
+    $("#postedComments").append( "<p id='last'></p>" );
+    dbg("Document Ready");
+
+    $(window).scroll(function() {
+        dbg("Window Scroll Start");
+
+        if (!doMouseWheel)
+            return;
+
+        var distanceTop = $('#last').offset().top - $(window).height();
+        if  ($(window).scrollTop() > distanceTop) {
+            dbg("Window distanceTop to scrollTop Start");
+            $('div#loadMoreComments').show();
+            doMouseWheel = 0 ;
+            
+            dbg("Another window to the end !!!! "+$(".postedComment:last").attr('id'));
+            $.ajax({
+                dataType : "html",
+                url: "jquery-loadMoreComments.php?lastComment="+ $(".postedComment:last").attr('id'),
+                success: function(html) {
+                    doMouseWheel = 1 ;
+                    if(html){
+                        $("#postedComments").append(html);
+                        dbg('Append html: ' + $(".postedComment:first").attr('id'));
+                        dbg('Append html: ' + $(".postedComment:last").attr('id'));
+
+                        $("#last").remove();
+                        $("#postedComments").append( "<p id='last'></p>" );
+                        $('div#loadMoreComments').hide();
+                    } else {
+                        //Disable Ajax when result from PHP-script is empty (no more DB-results )
+                        $('div#loadMoreComments').replaceWith( "<center><h1 style='color:red'>End of countries !!!!!!!</h1></center>" );
+                        doMouseWheel = 0 ;
+                    }
+                }
+            });
+        }
+    });
+});
+
+
